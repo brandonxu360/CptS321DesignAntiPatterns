589fbbf3fabe8dadc1ad86283a2246c4e1041e21
Brandon Xu <brandon.xu@wsu.edu>
2024-04-18 03:08:22 -0700

    Create command interface

---
+++ SpaghettiCodeDemoConsoleApp/ICommand.cs
@@ -0,0 +1,12 @@
+namespace SpaghettiCodeDemoConsoleApp;
+
+/// <summary>
+/// Command interface.
+/// </summary>
+public interface ICommand
+{
+    /// <summary>
+    /// Executes the command.
+    /// </summary>
+    void Execute();
+}g
